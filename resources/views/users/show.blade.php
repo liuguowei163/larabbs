f527cb4014890158f4b109c390adea6fff0a559c
liuguowei <liuguowei163@yeah.net>
2020-01-08 14:03:51 +0000

    注册用户默认头像

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,7 +14,10 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
             <hr>
             <h5><strong>注册于</strong></h5>
-            <p>January 01 1901</p>
+            <p>{{ $user->created_at->diffForHumans() }}</p>
+            <hr>
+            <h5><strong>最后活跃</strong></h5>
+            <p title="{{  $user->last_actived_at }}">{{ $user->last_actived_at->diffForHumans() }}</p>
       </div>
     </div>
   </div>
